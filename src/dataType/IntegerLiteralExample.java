67a668d89ba760e0c27418303503cf399287c563
Noman-sheikh1 <maanb645@gmail.com>
2026-03-08 23:05:33 +0530

    adding java literal

---
+++ src/dataType/IntegerLiteralExample.java
@@ -0,0 +1,16 @@
+package dataType;
+public class IntegerLiteralExample {
+
+    public static void main(String[] args) {
+
+        int a = 10;     // decimal
+        int b = 010;    // octal
+        int c = 0x10;   // hexadecimal
+        int d = 0b10;   // binary
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
