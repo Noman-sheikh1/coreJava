92288eca27e4a0bd7506e741b79e435b7272895b
Noman-sheikh1 <maanb645@gmail.com>
2026-03-10 23:13:14 +0530

    Looping  Statement in java

---
+++ src/Looping_Statement/do_While_Loop.java
@@ -0,0 +1,12 @@
+package Looping_Statement;
+
+public class do_While_Loop {
+    public static void main(String[] args){
+        System.out.println("this is do while loop:");
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+}
