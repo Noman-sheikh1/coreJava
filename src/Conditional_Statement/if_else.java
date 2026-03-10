853e56a3ad267d85953ab78ae8a3f40d0d768191
Noman-sheikh1 <maanb645@gmail.com>
2026-03-10 22:23:16 +0530

    Conditional Statement in java

---
+++ src/Conditional_Statement/if_else.java
@@ -0,0 +1,13 @@
+package Conditional_Statement;
+
+public class if_else {
+    public static void main(String args[])
+    {
+        int i = 10;
+
+        if (i < 15)
+            System.out.println("i is smaller than 15");
+        else
+            System.out.println("i is greater than 15");
+    }
+}
